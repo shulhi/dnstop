d99097efe21fc763856f323888029486565440fb
Shulhi Sapli <shulhi@gmail.com>
2015-01-06 21:34:56 +0800

    Copy relevant functions to printable version

--- dnstop.c
+++ dnstop.c
@@ -18,6 +18,8 @@ static const char *Version = "20120611";
 
 #include <netinet/in.h>
 
+#include <pthread.h>
+
 #include <pcap.h>
 #include <signal.h>
 #include <stdio.h>
@@ -1537,6 +1539,100 @@ report(void)
     if (SubReport)
         SubReport();
     refresh();
+}
+
+void
+Table_report_printable(SortItem * sorted, int rows, const char *col1, const char *col2, col_fmt F1, col_fmt F2, unsigned int base)
+{
+    int W1 = strlen(col1);
+    int W2 = col2 ? strlen(col2) : 0;
+    int WC = 9;                 /* width of "Count" column */
+    int WP = 6;                 /* width of "Percent" column */
+    int i;
+    int nlines = get_nlines();
+    int ncols = get_ncols();
+    char fmt1[64];
+    char fmt2[64];
+    unsigned int sum = 0;
+
+    if (nlines > rows)
+        nlines = rows;
+
+    for (i = 0; i < nlines; i++) {
+        const char *t = F1(sorted + i);
+        if (W1 < strlen(t))
+            W1 = strlen(t);
+    }
+    if (W1 + 1 + WC + 1 + WP + 1 + WP + 1 > ncols)
+        W1 = ncols - 1 - WC - 1 - WP - 1 - WP - 1;
+
+    if (NULL == col2 || NULL == F2) {
+        snprintf(fmt1, 64, "%%-%d.%ds %%%ds %%%ds %%%ds\n", W1, W1, WC, WP, WP);
+        snprintf(fmt2, 64, "%%-%d.%ds %%%dd %%%d.1f %%%d.1f\n", W1, W1, WC, WP, WP);
+        print_func(fmt1, col1, "Count", "%", "cum%");
+        print_func(fmt1, dashes(W1), dashes(WC), dashes(WP), dashes(WP));
+        for (i = 0; i < nlines; i++) {
+            sum += (sorted + i)->cnt;
+            const char *t = F1(sorted + i);
+            print_func(fmt2,
+                t,
+                (sorted + i)->cnt,
+                100.0 * (sorted + i)->cnt / base,
+                100.0 * sum / base);
+        }
+    } else {
+        for (i = 0; i < nlines; i++) {
+            const char *t = F2(sorted + i);
+            if (W2 < strlen(t))
+                W2 = strlen(t);
+        }
+        if (W2 + 1 + W1 + 1 + WC + 1 + WP + 1 + WP + 1 > ncols)
+            W2 = ncols - 1 - W1 - 1 - WC - 1 - WP - 1 - WP - 1;
+        snprintf(fmt1, 64, "%%-%d.%ds %%-%d.%ds %%%ds %%%ds %%%ds\n", W1, W1, W2, W2, WC, WP, WP);
+        snprintf(fmt2, 64, "%%-%d.%ds %%-%d.%ds %%%dd %%%d.1f %%%d.1f\n", W1, W1, W2, W2, WC, WP, WP);
+        print_func(fmt1, col1, col2, "Count", "%", "cum%");
+        print_func(fmt1, dashes(W1), dashes(W2), dashes(WC), dashes(WP), dashes(WP));
+        for (i = 0; i < nlines; i++) {
+            const char *t = F1(sorted + i);
+            const char *q = F2(sorted + i);
+            sum += (sorted + i)->cnt;
+            print_func(fmt2,
+                t,
+                q,
+                (sorted + i)->cnt,
+                100.0 * (sorted + i)->cnt / base,
+                100.0 * sum / base);
+        }
+    }
+}
+
+void
+AgentAddr_report_printable(hashtbl * tbl, const char *what)
+{
+    unsigned int sum = 0;
+    int sortsize = hash_count(tbl);
+    SortItem *sortme = calloc(sortsize, sizeof(SortItem));
+    AgentAddr *a;
+    hash_iter_init(tbl);
+    sortsize = 0;
+    while ((a = hash_iterate(tbl))) {
+        sum += a->count;
+        sortme[sortsize].cnt = a->count;
+        sortme[sortsize].ptr = a;
+        sortsize++;
+    }
+    qsort(sortme, sortsize, sizeof(SortItem), SortItem_cmp);
+    Table_report_printable(sortme, sortsize,
+        what, NULL,
+        AgentAddr_col_fmt, NULL,
+        sum);
+    free(sortme);
+}
+
+void
+printable_report(void)
+{
+        // Call to AgentAddr_report version printable
 }
 
 /*
